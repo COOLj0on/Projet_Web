e7c887df71c7dcb192ad07bf67f0da77fdd167d5
jdiaz2016 <jonathan.53970@hotmail.fr>
2017-06-21 12:02:03 +0200

    Auto stash before merge of "master" and "origin/master"
    modal builder bug

--- web/js/modalBuilder.js
+++ web/js/modalBuilder.js
@@ -1,10 +1,10 @@
 /**
  * Created by jdiaz2016 on 21/06/2017.
  */
-function getInput(name,label,type, placeholder=label, data=null){
+function getInput(name,label,type, placeholder, data){
     var output =$('<div class="form-group">')
-            .append($('<label for="'+name+'" class="col-lg-3 control-label">')).text(label)
-            .append($('<div class="col-lg-9">'))
-            .append($('<input value="'+data+'" type="text" class="form-control" name="'+name+'" id="'+name+'" placeholder="'+placeholder+'">'));
+            .append($('<label for="'+name+'" class="col-lg-3 control-label">').text(label))
+            .append($('<div class="col-lg-9">')
+            .append($('<input value="'+data+'" type="text" class="form-control" name="'+name+'" id="'+name+'" placeholder="'+placeholder+'">')));
     return output;
 }
